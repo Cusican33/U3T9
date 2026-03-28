dbafadc7e7853c59914249805af65bdd8aeb6aff
Cusican33 <Yariel.Perez66@gmail.com>
2026-03-28 14:55:51 -0400

    Finished part f

--- src/Main.java
+++ src/Main.java
@@ -25,14 +25,14 @@ public class Main
         System.out.println();
         System.out.println(b2.boxInfo() + "\n");
 
-        Box b1 = new Box(10);
+        b1 = new Box(10);
         Shape shape = new Shape();
         b1.addToShape(shape);
         Box shapeBox = shape.getBox();
         System.out.println(b1 == shapeBox);
         System.out.println(shapeBox.boxInfo());
         System.out.println();
-        Box b2 = new Box(20, 30, 40);
+        b2 = new Box(20, 30, 40);
         b2.addToShape(shape);
         shapeBox = shape.getBox();
         System.out.println(b2 == shapeBox);
